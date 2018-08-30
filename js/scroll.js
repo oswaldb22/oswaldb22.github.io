b9888c5aabd0f3a4044f6316cb27c8478a854abd
oswaldb22 <oswaldblassou@yahoo.fr>
2018-08-30 03:37:00 +0200

    Version2: adding emoji on homepage

--- js/scroll.js
+++ js/scroll.js
@@ -9,22 +9,20 @@ sr.reveal('.avatar',{
 
 sr.reveal('.text',{
   duration: 1000,
-  delay:1000,
+  delay:500,
   opacity: 0,
-  distance:'50px',
-  origin: 'left'
+
 })
 
 sr.reveal('.sub-text',{
   duration: 1000,
   opacity: 0,
-  delay:1000,
-  distance:'100px',
-  origin: 'left'
+  delay:500,
+
 })
 
 sr.reveal('.button',{
-  duration: 1000,
+  duration: 800,
   opacity: 0,
   delay:1000,
 })
