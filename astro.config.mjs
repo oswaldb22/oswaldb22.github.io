e3cb612adddcff9c46bb9734fc571c6138c9180c
Oswald BLASSOU <oswaldblassou@yahoo.fr>
2025-04-10 02:37:16 +0200

    feat: deploy to github pages action and settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://oswaldb22.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
 });
