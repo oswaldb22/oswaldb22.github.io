5165a48d65be63c04d200b93b04af823c9abdf85
Oswald BLASSOU <oswaldblassou@yahoo.fr>
2025-04-10 02:38:27 +0200

    feat: updated description of home page

--- src/consts.ts
+++ src/consts.ts
@@ -10,7 +10,7 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION: "oswaldb22's personal website.",
 };
 
 export const BLOG: Metadata = {
