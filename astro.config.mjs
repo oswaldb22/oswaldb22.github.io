7f6d8df225ed25baeb5a97b03de2e1ad8638f517
Oswald BLASSOU <oswaldblassou@yahoo.fr>
2026-04-17 00:21:52 +0200

    feat: changed site to com

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://oswaldb22.github.io",
+  site: "https://oswaldb22.com",
   integrations: [mdx(), sitemap(), tailwind()],
 });
